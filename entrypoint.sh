63e7e646eea588dcee13cfc1e429682c4aa77e2a
Bahtah <btolikov@gmail.com>
2023-11-02 16:41:26 +0600

    refactor entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ python -m celery -A wiser_load_board beat --loglevel=info &
 
 
 # Start your Django application
+# shellcheck disable=SC2093
 exec /bin/sh -c "python manage.py migrate && gunicorn --bind :8080 wiser_load_board.wsgi:application"
 
 chmod +x entrypoint.sh
