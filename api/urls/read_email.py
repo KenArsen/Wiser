c7d83086a0958ca34995dfb37085643f8aee9657
Bahtah <btolikov@gmail.com>
2023-11-04 21:14:24 +0600

    add urls

--- api/urls/read_email.py
+++ api/urls/read_email.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from apps.read_email.parser import read_gmail
+from api.views.read_email import OrderView
 
 urlpatterns = [
-    path('read-emails/', read_gmail),
+    path('read-email/', read_gmail),
+    path('', OrderView.as_view()),
 ]
