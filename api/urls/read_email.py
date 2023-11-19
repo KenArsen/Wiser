707843350fb423c69944a20c962180c7e6a57b7e
Bahtah <btolikov@gmail.com>
2023-11-19 23:49:37 +0600

    add_filters

--- api/urls/read_email.py
+++ api/urls/read_email.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.read_email.parser import read_gmail
-from api.views.read_email import OrderView
+from apps.read_email import views
+from api.views.read_email import OrderView, OrderFilterView
 
 router = DefaultRouter()
 router.register(r'orders', OrderView)
 
 urlpatterns = [
     #path('read-email/', read_gmail, name='read-email'),
+    path('orders/filter/', OrderFilterView.as_view(), name='order-filter'),
     path('', include(router.urls)),
 ]
--- api/views/read_email.py
+++ api/views/read_email.py
@@ -3,9 +3,28 @@ from rest_framework import viewsets
 from apps.read_email.models import Order
 from api.serializers.read_email import OrderSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 class OrderView(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
 
+class OrderFilterView(APIView):
+    def get(self, request):
+        pick_up_at = request.query_params.get('pick_up_at')
+        deliver_to = request.query_params.get('deliver_to')
+        miles = request.query_params.get('miles')
+
+        filtered_orders = Order.objects.all()
+        if pick_up_at:
+            filtered_orders = filtered_orders.filter(pick_up_at__icontains=pick_up_at)
+        if deliver_to:
+            filtered_orders = filtered_orders.filter(deliver_to__icontains=deliver_to)
+        if deliver_to:
+            filtered_orders = filtered_orders.filter(miles__contains=miles)
+
+        serialized_data = OrderSerializer(filtered_orders, many=True)
+        return Response(serialized_data.data)
