01352ccaa481fabf8ba431c2a666f3abce4635c4
Bahtah <btolikov@gmail.com>
2023-11-23 16:37:10 +0600

    driver add

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
-from django.db.models.signals import pre_delete, post_migrate
+from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 import os
 from django.utils.translation import gettext_lazy as _
