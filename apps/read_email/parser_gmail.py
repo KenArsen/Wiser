fe4d0dd20797da8dd00307df0a8e042112468126
KenArsen <kenzhegulov2002@gmail.com>
2024-01-27 13:00:49 +0600

    Replaced the line that decrypts the contents of the email

--- apps/read_email/parser_gmail.py
+++ apps/read_email/parser_gmail.py
@@ -30,16 +30,16 @@ def process_and_save_emails():
             if msg.is_multipart():
                 for part in msg.walk():
                     if part.get_content_type() == "text/html":
-                        body = part.get_payload(decode=True).decode()
+                        body = part.get_payload(decode=True).decode('utf-8', errors='replace')  # change
             else:
-                body = msg.get_payload(decode=True).decode()
+                body = msg.get_payload(decode=True).decode('utf-8', errors='replace')  # change
 
             if not body:
                 print("Пустое письмо")
                 continue
 
             try:
-                soup = BeautifulSoup(body, "lxml")
+                soup = BeautifulSoup(body, "lxml", from_encoding='utf-8')  # change
                 br_tags = soup.find_all('br')
                 for br_tag in br_tags:
                     br_tag.replace_with('\n')
