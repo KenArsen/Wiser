62240b12b4751be81b91f7ac630574674e4bba57
KenArsen <kenzhegulov2002@gmail.com>
2024-01-31 13:39:20 +0600

    add Exsepsion for send mail

--- apps/letter/tasks.py
+++ apps/letter/tasks.py
@@ -1,3 +1,5 @@
+from smtplib import SMTPException, SMTPAuthenticationError
+
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
@@ -7,12 +9,20 @@ from django.utils.html import strip_tags
 
 @shared_task
 def send_mail_to_order(letter):
-    email_body = render_to_string('send_email.html', {'letter': letter})
-    email = EmailMultiAlternatives(
-        subject='',
-        body=strip_tags(email_body),
-        from_email=settings.EMAIL_HOST_USER,
-        to=['tan.me4nik@gmail.com']
-    )
-    email.attach_alternative(email_body, 'text/html')
-    email.send()
+    try:
+        email_body = render_to_string('send_email.html', {'letter': letter})
+        email = EmailMultiAlternatives(
+            subject='',
+            body=strip_tags(email_body),
+            from_email=settings.EMAIL_HOST_USER,
+            to=['tan.me4nik@gmail.com']
+        )
+        email.attach_alternative(email_body, 'text/html')
+        email.send()
+        print('Сообщение успешно отправлено')
+    except SMTPAuthenticationError:
+        print('Ошибка аутентификации SMTP. Проверьте настройки почты.')
+    except SMTPException as e:
+        print(f'Ошибка SMTP: {e}')
+    except Exception as e:
+        print(f'Общая ошибка при отправке почты: {e}')
--- apps/letter/views.py
+++ apps/letter/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
-from rest_framework import status, viewsets
 from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status, viewsets
 
 from api.utils.permissions import IsDispatcher, IsAdmin
 from apps.read_email.models import Order
@@ -20,28 +21,28 @@ class LetterViewSet(viewsets.ModelViewSet):
 @permission_classes([IsAuthenticated, IsAdmin | IsDispatcher])
 @action(methods=["GET"], detail=False)
 def send_mail(request, order_id, driver_id, rate):
+    order = get_object_or_404(Order, pk=order_id)
+
+    letter = create_letter(order, rate)
+    serializer = LetterSerializer(data=letter.__dict__)
+
+    if serializer.is_valid():
+        serializer.save()
+        send_mail_to_order.delay(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def create_letter(order, rate):
     letter = Letter()
-    try:
-        order = Order.objects.get(pk=order_id)
-        letter.rate = rate
-        letter.dims = order.dims
-        letter.mc = '1220386'
-        letter.miles = order.miles
-        letter.eta_to_pick_up = get_delivery_time(order)
-        letter.dock_high = True
-        letter.account = order.from_whom
-
-        serializer = LetterSerializer(data=letter.__dict__)
-
-        if serializer.is_valid():
-            serializer.save()
-
-            send_mail_to_order.delay(serializer.data)
-
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    except Order.DoesNotExist:
-        return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+    letter.rate = rate
+    letter.dims = order.dims
+    letter.mc = '1220386'
+    letter.miles = order.miles
+    letter.eta_to_pick_up = get_delivery_time(order)
+    letter.dock_high = True
+    letter.account = order.from_whom
+    return letter
 
 
 def get_delivery_time(order):
@@ -51,15 +52,12 @@ def get_delivery_time(order):
         return {"error": "Delivery time not specified or order is not active."}
 
     current_time = timezone.localtime(timezone.now())
-
     time_until_delivery = delivery_time - current_time
 
     total_seconds = time_until_delivery.total_seconds()
-
     days, remainder = divmod(total_seconds, 86400)
     hours, remainder = divmod(remainder, 3600)
     minutes, _ = divmod(remainder, 60)
-
     hours += days * 24
 
     return f"{int(hours)}:{int(minutes)}"
