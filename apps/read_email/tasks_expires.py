c2e4fac20670e963f1a297939d89335a3c587e37
KenArsen <kenzhegulov2002@gmail.com>
2024-02-07 09:19:41 +0600

    removed time check when deleting an order

--- apps/read_email/tasks_expires.py
+++ apps/read_email/tasks_expires.py
@@ -6,9 +6,8 @@ from .models import Order
 
 @shared_task
 def deactivate_expired_order(order_id):
-    current_time = timezone.localtime(timezone.now())
     try:
-        order = Order.objects.get(pk=order_id, is_active=True, this_posting_expires_est__lte=current_time)
+        order = Order.objects.get(pk=order_id, is_active=True)
         if order.user is None:
             print(f"Время действия заказа: {order.order_number} истекло в: {order.this_posting_expires_est}")
             order.delete()
@@ -17,5 +16,5 @@ def deactivate_expired_order(order_id):
             print(f"Время действия заказа {order.order_number} истекло. Заказ перемещен в историю")
             order.is_active = False
             order.save()
-    except Order.DoesNotExist:
-        pass
+    except Order.DoesNotExist as e:
+        print(f'Ошибка на deactivate_expired_order {e}')
