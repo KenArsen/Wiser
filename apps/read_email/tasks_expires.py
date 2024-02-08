4624ac8f9a93475bfdb637574137945e3c20f6b9
KenArsen <kenzhegulov2002@gmail.com>
2024-02-08 20:50:39 +0600

    add transactions again

--- apps/read_email/tasks_expires.py
+++ apps/read_email/tasks_expires.py
@@ -10,8 +10,8 @@ def deactivate_expired_order(order_id):
         order = Order.objects.get(pk=order_id)
         if order.user is None:
             print(f"Время действия заказа: {order.id} истекло в: {order.this_posting_expires_est}")
-            order.delete()
             print(f"Заказ с номером {order.order_number} удален")
+            order.delete()
         else:
             print(f"Время действия заказа {order.order_number} истекло. Заказ перемещен в историю")
             order.is_active = False
