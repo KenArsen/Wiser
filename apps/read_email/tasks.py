c3a85476c925ceab974802e7c057023cacda8400
KenArsen <kenzhegulov2002@gmail.com>
2024-02-12 22:57:37 +0600

    delete every 3 minutes

--- apps/read_email/tasks.py
+++ apps/read_email/tasks.py
@@ -11,18 +11,18 @@ def process_and_save_emails_task():
     return process_and_save_emails()
 
 
-# @shared_task()
-# def delete_expired_data():
-#     logging.info('########## Удаление начинался ##########')
-#     active_orders = Order.objects.filter(Q(this_posting_expires_est__lt=timezone.now()), ~Q(user=None))
-#     if active_orders.exists():
-#         logging.info(f"Время действия {active_orders.count()} заказов истекло. Перемещаем в историю...")
-#         active_orders.update(is_active=False)
-#         logging.info("Заказы перемещены в историю")
-#
-#     expired_orders = Order.objects.filter(this_posting_expires_est__lt=timezone.now(), user=None)
-#     if expired_orders.exists():
-#         logging.info(f"Время действия {expired_orders.count()} заказов истекло. Удаляем записи...")
-#         expired_orders.delete()
-#         logging.info("Заказы удалены")
-#     logging.info('########## Удаление закончился ##########')
+@shared_task()
+def delete_expired_data():
+    logging.info('########## Удаление начинался ##########')
+    active_orders = Order.objects.filter(Q(this_posting_expires_est__lt=timezone.now()), ~Q(user=None))
+    if active_orders.exists():
+        logging.info(f"Время действия {active_orders.count()} заказов истекло. Перемещаем в историю...")
+        active_orders.update(is_active=False)
+        logging.info("Заказы перемещены в историю")
+
+    expired_orders = Order.objects.filter(this_posting_expires_est__lt=timezone.now(), user=None)
+    if expired_orders.exists():
+        logging.info(f"Время действия {expired_orders.count()} заказов истекло. Удаляем записи...")
+        expired_orders.delete()
+        logging.info("Заказы удалены")
+    logging.info('########## Удаление закончился ##########')
