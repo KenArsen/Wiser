8d087fbc30f6b6cb7f347efae66d62f810989869
KenArsen <kenzhegulov2002@gmail.com>
2024-02-12 18:24:43 +0600

    add again transaction

--- apps/read_email/tasks.py
+++ apps/read_email/tasks.py
@@ -9,11 +9,11 @@ def process_and_save_emails_task():
     return process_and_save_emails()
 
 
-@shared_task()
-def delete_expired_data():
-    from datetime import datetime
-    from apps.read_email.models import Order
-
-    logging.info('##### Удаление данные которые истекло время #####')
-    expired_data = Order.objects.filter(this_posting_expires_est__lt=datetime.now())
-    expired_data.delete()
+# @shared_task()
+# def delete_expired_data():
+#     from datetime import datetime
+#     from apps.read_email.models import Order
+#
+#     logging.info('##### Удаление данные которые истекло время #####')
+#     expired_data = Order.objects.filter(this_posting_expires_est__lt=datetime.now())
+#     expired_data.delete()
