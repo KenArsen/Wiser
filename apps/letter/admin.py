8a4de533936a5b6780e91ef2d7b5528b465d188e
KenArsen <kenzhegulov2002@gmail.com>
2024-04-11 13:34:50 +0600

    Changed Letter __str__ and admin panel

--- apps/letter/admin.py
+++ apps/letter/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Letter
 
-admin.site.register(Letter)
+
+@admin.register(Letter)
+class LetterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order_id', 'driver_id')
+    list_display_links = ('id', 'order_id', 'driver_id')
+    readonly_fields = ('created_at', 'updated_at')
