e1674f7af3306f3b354d312c259d206438f85b19
KenArsen <kenzhegulov2002@gmail.com>
2024-04-11 13:28:10 +0600

    Changed driver admin panel

--- apps/driver/admin.py
+++ apps/driver/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Driver
 
-admin.site.register(Driver)
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email',)
+    list_display_links = ('id', 'email')
+    readonly_fields = ('created_at', 'updated_at')
