10b7d76621d5eeadcdd198ace803360c1ca8fb52
KenArsen <kenzhegulov2002@gmail.com>
2024-05-01 12:06:38 +0600

    Get location

--- apps/order/api/v1/apis/get_location_apis.py
+++ apps/order/api/v1/apis/get_location_apis.py
@@ -3,7 +3,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status, views
 from rest_framework.response import Response
 
-from apps.driver.models import Driver
 from apps.order.models import Order
 from apps.vehicle.models import Vehicles
 
@@ -11,8 +10,10 @@ from apps.vehicle.models import Vehicles
 class GetLocationAPI(views.APIView):
     def post(self, request):
         order_id = request.data.get("order_id")
-        driver_id = request.data.get("driver_id")
-        radius = int(request.data.get("radius", 100))
+        radius = int(request.data.get("radius", 1000))
+
+        url = "https://api.geoapify.com/v1/routematrix?apiKey=4d1a57181b7d427ebacdf105eced5c1d"
+        headers = {"Content-Type": "application/json"}
 
         if not order_id:
             return Response({"error": "Order ID is required."}, status=status.HTTP_400_BAD_REQUEST)
@@ -20,41 +21,33 @@ class GetLocationAPI(views.APIView):
         order = get_object_or_404(Order, pk=order_id)
         lat_order, lon_order = order.coordinate_from.split(",")
 
-        if driver_id:
-            driver = get_object_or_404(Driver, id=driver_id)
-            vehicle = driver.vehicle
-            lat_driver, lon_driver = vehicle.coordinate_from.split(",")
-            distance_km, time = self.get_distance_and_time(lon_order, lat_order, lon_driver, lat_driver)
-            driver_info = self.get_driver_info(driver, distance_km, time)
-            return Response(
-                {"message": "Driver assigned successfully.", "selected_driver_info": driver_info},
-                status=status.HTTP_200_OK,
-            )
+        sources = f'"location":[{lat_order},{lon_order}]'
+        targets = ""
 
-        drivers = []
         vehicles = Vehicles.objects.all()
+        drivers = []
         for vehicle in vehicles:
+            drivers.append(vehicle.driver)
             lat_driver, lon_driver = vehicle.coordinate_from.split(",")
-            distance_miles, time = self.get_distance_and_time(lon_order, lat_order, lon_driver, lat_driver)
-            if distance_miles <= radius:
-                driver = Driver.objects.get(email=vehicle.driver)
-                driver_info = self.get_driver_info(driver, distance_miles, time)
-                drivers.append(driver_info)
+            targets += '{"location":[' + lat_driver + "," + lon_driver + "]},"
+        targets = targets[:-1]
+
+        data = '{"mode":"drive","sources":[{' + sources + '}],"targets":[' + targets + "]}"
+        resp = requests.post(url, headers=headers, data=data)
+
+        resp_json = resp.json()
+        sources_to_targets = resp_json["sources_to_targets"][0]
+        available_drivers = []
+        for i in range(len(sources_to_targets)):
+            distance = sources_to_targets[i]["distance"]
+            time = sources_to_targets[i]["time"]
+            if distance <= radius:
+                available_drivers.append(self.get_driver_info(drivers[i], distance, time))
 
         return Response(
-            {"order_coordinates": order.coordinate_from, "available_drivers": drivers}, status=status.HTTP_200_OK
+            {"order_coordinates": order.coordinate_from, "available_drivers": available_drivers},
+            status=status.HTTP_200_OK,
         )
-
-    def get_distance_and_time(self, lon_order, lat_order, lon_driver, lat_driver):
-        url = (
-            f"https://routing.openstreetmap.de/routed-car/route/v1/driving/"
-            f"{lon_order},{lat_order};{lon_driver},{lat_driver}?overview=false&geometries=polyline&steps=true"
-        )
-        response = requests.get(url)
-        data = response.json()
-        distance_miles = data.get("routes")[0]["distance"] / 1000 * 0.62137119
-        time = distance_miles / 55.0
-        return distance_miles, time
 
     def get_driver_info(self, driver, distance_miles, time):
         return {
@@ -62,7 +55,7 @@ class GetLocationAPI(views.APIView):
             "first_name": driver.first_name,
             "last_name": driver.last_name,
             "phone_number": driver.phone_number,
-            "miles_out": round(distance_miles, 1),
-            "time": round(time, 2),
+            "miles_out": distance_miles,
+            "time": time,
             "coordinate_from": driver.vehicle.coordinate_from,
         }
--- apps/order/tasks.py
+++ apps/order/tasks.py
@@ -19,33 +19,32 @@ from .models import Order
 
 @shared_task()
 def get_location():
-    logging.info(f'{"#" * 10} Getting location start {"#" * 10}')
-    orders = Order.objects.all()
-    for order in orders:
-        lat_order, lon_order = order.coordinate_from.split(",")
-
-        vehicles = Vehicles.objects.all()
-        match = 0
-        for vehicle in vehicles:
-            lat_driver, lon_driver = vehicle.coordinate_from.split(",")
-            distance_miles = get_distance_and_time(lon_order, lat_order, lon_driver, lat_driver)
-            if distance_miles <= 1000:
-                match += 1
-
-        order.match = match
-        order.save()
-    logging.info(f'{"#" * 10} Getting location end {"#" * 10}')
-
-
-def get_distance_and_time(lon_order, lat_order, lon_driver, lat_driver):
-    url = (
-        f"https://routing.openstreetmap.de/routed-car/route/v1/driving/"
-        f"{lon_order},{lat_order};{lon_driver},{lat_driver}?overview=false&geometries=polyline&steps=true"
-    )
-    response = requests.get(url)
-    data = response.json()
-    distance_miles = data.get("routes")[0]["distance"] / 1000 * 0.62137119
-    return distance_miles
+    pass
+    # logging.info(f'{"#" * 10} Getting location start {"#" * 10}')
+    # orders = Order.objects.all()
+    # for order in orders:
+    #     lat_order, lon_order = order.coordinate_from.split(",")
+    #
+    #     match = 0
+    #     sources = f'"location":[{lat_order},{lon_order}]'
+    #     targets = ''
+    #     vehicles = Vehicles.objects.all()
+    #     drivers = []
+    #     for vehicle in vehicles:
+    #         drivers.append(vehicle.driver)
+    #         lat_driver, lon_driver = vehicle.coordinate_from.split(",")
+    #         targets += '{"location":[' + lat_driver + ',' + lon_driver + ']},'
+    #     targets = targets[:-1]
+    #
+    #     for vehicle in vehicles:
+    #         lat_driver, lon_driver = vehicle.coordinate_from.split(",")
+    #         distance_miles = get_distance_and_time(lon_order, lat_order, lon_driver, lat_driver)
+    #         if distance_miles <= 1000:
+    #             match += 1
+    #
+    #     order.match = match
+    #     order.save()
+    # logging.info(f'{"#" * 10} Getting location end {"#" * 10}')
 
 
 @shared_task()
