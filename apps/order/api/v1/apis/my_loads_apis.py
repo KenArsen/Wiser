3f22719808deb3f1cbfece334c451c4ea29193d9
KenArsen <kenzhegulov2002@gmail.com>
2024-05-16 13:24:33 +0600

    Remove checkout status is filter my_loads_history

--- apps/order/api/v1/apis/my_loads_apis.py
+++ apps/order/api/v1/apis/my_loads_apis.py
@@ -27,7 +27,7 @@ class MyLoadsHistoryAPI(generics.ListAPIView):
 
     def get_queryset(self):
         return Order.objects.filter(
-            Q(order_status="REFUSED", assign__isnull=False) | Q(order_status="CHECKOUT") | Q(order_status="COMPLETED"),
+            Q(order_status="REFUSED", assign__isnull=False) | Q(order_status="COMPLETED"),
         )
 
 
