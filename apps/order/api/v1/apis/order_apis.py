fdb3a17caf1d62ed6a3b94dfa6d16520e42fdfec
KenArsen <kenzhegulov2002@gmail.com>
2024-05-16 12:11:10 +0600

    done1

--- apps/order/api/v1/apis/order_apis.py
+++ apps/order/api/v1/apis/order_apis.py
@@ -81,13 +81,16 @@ class OrderRefuseAPI(generics.GenericAPIView):
 
 
 class MyLoadSet(generics.GenericAPIView):
-    queryset = Order.objects.filter(Q(order_status='AWAITING_BID') | Q(order_status='PENDING'))
+    queryset = Order.objects.all()
 
     def post(self, request, *args, **kwargs):
         for order in self.queryset.all():
-            order.my_load_status.previous_status = order.my_load_status.Status.POINT_A
-            order.my_load_status.current_status = None
-            order.my_load_status.next_status = None
+            if order.my_load_status.previous_status == "0":
+                order.my_load_status.previous_status = None
+            if order.my_load_status.current_status == "0":
+                order.my_load_status.current_status = None
+            if order.my_load_status.next_status == "0":
+                order.my_load_status.next_status = None
             order.my_load_status.save()
         return Response(status=status.HTTP_200_OK)
 
