b62817a7c85a9e7e5f05a507b0093c22d8300851
KenArsen <kenzhegulov2002@gmail.com>
2024-05-22 14:50:20 +0600

    Bug OrderService

--- apps/order/services/__init__.py
+++ apps/order/services/__init__.py
@@ -1,4 +1,4 @@
+from .order_service import OrderService
 from .load_board_service import LoadBoardService
 from .my_bid_service import MyBidService
 from .my_load_service import MyLoadService
-from .order_service import OrderService
