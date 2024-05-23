22b5f369617112ade98d129b680be87e8ede2a83
KenArsen <kenzhegulov2002@gmail.com>
2024-05-23 17:24:55 +0600

    Bug in assing function

--- apps/order/services/my_bid_service.py
+++ apps/order/services/my_bid_service.py
@@ -1,4 +1,5 @@
 from rest_framework.exceptions import ValidationError
+from django.db import IntegrityError
 
 from apps.order.models import MyLoadStatus
 
@@ -16,24 +17,33 @@ class MyBidService(OrderService):
     def assign(self, data):
         order = self._get_order(data=data)
         order.order_status = "ACTIVE"
-        order.my_load_status.current_status = MyLoadStatus.Status.POINT_A
-        order.my_load_status.next_status = MyLoadStatus.Status.UPLOADED
-        order.my_load_status.save()
+
+        try:
+            MyLoadStatus.objects.create(
+                order=order,
+                current_status=MyLoadStatus.Status.POINT_A,
+                next_status=MyLoadStatus.Status.UPLOADED
+            )
+        except IntegrityError:
+            raise ValidationError({"detail": "Order already assigned."})
+
         order.save()
-        if hasattr(order, "assign"):
-            order.assign.delete()
-        broker_price = data.get("broker_price", None)
-        driver_price = data.get("driver_price", None)
+
+        broker_price = data.get("broker_price")
+        driver_price = data.get("driver_price")
+
         if broker_price is not None:
             order.letter.broker_price = broker_price
-            order.letter.save()
+            order.letter.save(update_fields=['broker_price'])
+
         if driver_price is not None:
             order.letter.driver_price = driver_price
-            order.letter.save()
+            order.letter.save(update_fields=['driver_price'])
 
         serializer = self.serializer(data=data)
         if serializer.is_valid():
             serializer.save()
         else:
-            raise ValidationError({"error": "Broker company/Rate confirmation not is valid"})
+            raise ValidationError({"error": "Broker company/Rate confirmation is not valid"})
+
         return {"detail": "The order has been moved to My Loads"}
